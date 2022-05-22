c007eb5eda19d43fa2e9a2997f8e9df3d3061317
AlbertGk <albertgk01@gmail.com>
2022-05-22 16:31:44 +0200

    refactor

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,85 @@
+import { Component } from 'react';
 
-export const App = ({ children }) => {
-  return <div>{children}</div>;
-};
+import { Section } from 'components/Section';
+import { FeedbackOptions } from 'components/FeedbackOptions';
+import { Statistics } from 'components/Statistics';
+
+class App extends Component {
+  static defaultProps = {
+    mark: 1,
+  };
+
+  static propTypes = {};
+
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  increaseGoodMark = ev => {
+    this.setState((state, props) => ({
+      good: state.good + props.mark,
+    }));
+  };
+
+  increaseNeutralMark = ev => {
+    this.setState((state, props) => ({
+      neutral: state.neutral + props.mark,
+    }));
+  };
+
+  increaseBadMark = ev => {
+    this.setState((state, props) => ({
+      bad: state.bad + props.mark,
+    }));
+  };
+
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+  countPositiveFeedbackPercentage = () => {
+    return Math.round(
+      (100 * this.state.good) /
+        (this.state.good + this.state.neutral + this.state.bad)
+    );
+  };
+
+  render() {
+    const { good } = this.state;
+    const { neutral } = this.state;
+    const { bad } = this.state;
+
+    return (
+      <>
+        <Section title="Feedback">
+          <h1>Please leave feedback</h1>
+          <FeedbackOptions
+            options="Good"
+            onLeaveFeedback={this.increaseGoodMark}
+          ></FeedbackOptions>
+          <FeedbackOptions
+            options="Neutral"
+            onLeaveFeedback={this.increaseNeutralMark}
+          ></FeedbackOptions>
+          <FeedbackOptions
+            options="Bad"
+            onLeaveFeedback={this.increaseBadMark}
+          ></FeedbackOptions>
+        </Section>
+        <Section title="Statistics">
+          <h1>Statistics</h1>
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={this.countTotalFeedback()}
+            positivePercentage={this.countPositiveFeedbackPercentage()}
+          ></Statistics>
+        </Section>
+      </>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import App from 'components/App';
 import './index.css';
-import Feedback from 'components/Feedback';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App><Feedback></Feedback></App>
+    <App/>
   </React.StrictMode>
 );
