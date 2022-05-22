ec73727554b20a9f56a2709e8972c0a911044a0b
AlbertGk <albertgk01@gmail.com>
2022-05-22 12:03:50 +0200

    good, neutral, bad states handlers

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import Feedback from 'components/Feedback';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App><Feedback></Feedback></App>
   </React.StrictMode>
 );
